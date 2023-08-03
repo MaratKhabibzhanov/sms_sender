e6ddde3a0606d3b108de052a20703a9687574629
ascren <marat.khabibzhanov@mail.ru>
2023-08-03 10:06:52 +0500

    finish logic

--- sender/sms_sender/tasks.py
+++ sender/sms_sender/tasks.py
@@ -1,8 +1,13 @@
-from django.utils import timezone
-
 from celery import shared_task
 from django.db.models import Q
+from django.utils import timezone
+from smsaero import SmsAero
+
 from sms_sender.models import Mailing, Client, Report
+
+
+SMSAERO_EMAIL = 'skylinegtr_r34@mail.ru'
+SMSAERO_API_KEY = 't_eXAlYUE35w3wz-ytOD3z_LqbCjS25z'
 
 
 @shared_task()
@@ -12,6 +17,25 @@ def make_sms_send(mailing_id):
     clients = Client.objects.filter(Q(teg__in=filter_data) |
                                      Q(operator_code__in=filter_data))
     for client in clients:
-        Report.objects.create(message=mailing.message, client=client)
+        result = send_sms(client.phone, str(mailing.message))
+        if result.get('id'):
+            Report.objects.create(message=mailing.message, client=client)
         if timezone.now() >= mailing.date_end:
             break
+
+
+def send_sms(phone: int, message: str) -> dict:
+    """
+    Send sms and return results.
+
+            Parameters:
+                    phone (int): Phone number
+                    message (str): Message to send
+
+            Returns:
+                    data (dict): API request result
+    """
+    api = SmsAero(SMSAERO_EMAIL, SMSAERO_API_KEY)
+    res = api.send(phone, message)
+    assert res.get('success'), res.get('message')
+    return res.get('data')
